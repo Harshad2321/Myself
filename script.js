9e6d69e0a1d258921d15dc7b6a771f6680b00787
Harshad Agrawal <harshad.agrawal2005@gmail.com>
2025-07-18 23:43:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-AOS.init();
 
-const toggle = document.getElementById('toggle-mode');
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
-
-// Modal logic
-function openModal(certName) {
-  const modal = document.getElementById("modal");
-  const content = document.getElementById("modal-content");
-  content.src = `assets/certificates/${certName}`;
-  modal.style.display = "block";
-}
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
